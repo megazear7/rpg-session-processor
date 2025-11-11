37aa3069e063d6a7e80604372bb0dd32b9831172
Alex Lockhart <alexlockhart7@protonmail.com>
2025-11-10 22:33:31 -0500

    minor updates

--- src/prompt.dm-notes.ts
+++ src/prompt.dm-notes.ts
@@ -6,6 +6,7 @@ You have been given an overview containing the key events from a TTRPG session.
 Focus on the most important details that the game master needs to remember for future sessions.
 Take note characters that died or were introduced, loot acquired, experience gained, and important plot points.
 Use clear and concise language to create a summary that is easy to read and understand and is written in paragraph form.
+Focus on how the session ended so that the game master can pick up where they left off.
 It should be about ${length} words in length.
 Do not mention the word count in the summary and do not include any "Here is your summary" preamble.
 `.trim();
--- src/prompt.play-by-play.ts
+++ src/prompt.play-by-play.ts
@@ -6,7 +6,6 @@ You have been given an overview containing the key events from a TTRPG session.
 Your task is to provide a concise play by play of the session.
 Focus on the players, their actions, and events in the game at the table, not the in-world story.
 Use short sentences and simple language so it is easy for the game master to quickly review what happened.
-Do not mention particular dice rolls.
 Focus on the meta aspects of the session rather than the in-world narrative.
 Focus on details that will be important for the game master to remember for future sessions.
 This could include player decisions, characters that died or were introduced, loot acquired, experience gained, and important plot points.
